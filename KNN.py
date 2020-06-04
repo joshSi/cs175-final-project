92c004d76157363aa352d8232ec145651a513d15
Nomin Amgalan <namgalan@uci.edu>
2020-06-04 10:03:17 -0700

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -4,10 +4,16 @@
 
 import numpy as np 
 import pandas as pd 
-import sklearn.neighbors as neighbours
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsRegressor as KNN
 
+
+def MSE(Y, Yhat):
+    return np.sum((Y-Yhat)**2)
 
 data = np.genfromtxt('final.csv', delimiter = ",")
+np.random.shuffle(data)
+
 X = data[1:data.shape[0]-1, 1:7]
 Y = data[1:data.shape[0]-1, 7:8]
 tr = int(X.shape[0]*0.8)
@@ -17,4 +23,15 @@ Xte = X[tr:tr+te]
 Ytr = Y[0:tr]
 Yte = Y[tr:tr+te]
 
+k_values = np.arange(1, 600, 3, dtype = int)
+errors = []
+for k in k_values:
+    knn = KNN(n_neighbors=k)
+    knn.fit(Xtr, Ytr)
+    Yhat = knn.predict(Xte)
+    print("K:", k, "MSE:", MSE(Yte, Yhat))
+    errors.append(MSE(Yte, Yhat))
+
+#plt.plot(np.arange(1, 201), errors)
+#plt.show()
 
